de52e3d557563bacdb18eb4ebf1f80842d251ad7
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-16 00:28:48 +0300

    Create func_overl01.cpp

---
+++ source_code/func_overl01.cpp
@@ -0,0 +1,34 @@
+// func_overl01.cpp
+//
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int add(int x, int y) {
+	return x + y;
+}
+
+double add(double x, double y) {
+	return x + y;
+}
+
+string add(string x, string y) {
+	return x + y;
+}
+
+int main() {
+	
+	int integer_sum = add(1, 2);
+	double double_sum = add(5.1, 3.25);
+	string string_add = add("Mia fora kai enan kairo, ", "pige i gata ston xoro");
+	
+	cout << "The results are: \n";
+	cout << "Integer version -> " << integer_sum << "\n";
+	cout << "Double version -> " << double_sum << "\n";
+	cout << "String version -> " << string_add << "\n";
+
+	return 0;
+}
